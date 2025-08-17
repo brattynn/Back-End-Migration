50829c92796336d1840a4863f1489ce89e77d2b6
Brattynn <brattynndev@icloud.com>
2025-08-16 23:28:36 -0400

    Initial commit to Third_Branch, restarting project.

---
+++ src/main/java/com/d288/bth/BthApplication.java
@@ -0,0 +1,13 @@
+package com.d288.bth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d288/bth/BthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.bth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
