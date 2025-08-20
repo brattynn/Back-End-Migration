9151a248a3f44ba05ea94e35182b4e21736174af
Brattynn T <brattynndev@icloud.com>
2025-08-19 19:16:35 -0700

    Swapped the customer phone and zipcode entries as I entered them backwards for a few customers.

--- src/main/java/com/d288/d288/bootstrap/BootStrapData.java
+++ src/main/java/com/d288/d288/bootstrap/BootStrapData.java
@@ -33,38 +33,38 @@ public class BootStrapData implements CommandLineRunner {
             matt.setAddress("123 Sing Road");
             matt.setPostal_code("12345");
             matt.setPhone("111-555-0123");
-            matt.setDivision(divisionRepository.findAll().get(1));
+            matt.setDivision(divisionRepository.findAll().get(7));
 
             Customer corey = new Customer();
             corey.setFirstName("Corey");
             corey.setLastName("Beaulieu");
             corey.setAddress("234 Guitar Street");
-            corey.setPhone("23456");
-            corey.setPostal_code("222-444-1234");
+            corey.setPhone("222-444-1234");
+            corey.setPostal_code("23456");
             corey.setDivision(divisionRepository.findAll().get(2));
 
             Customer alex = new Customer();
             alex.setFirstName("Alex");
             alex.setLastName("Bent");
             alex.setAddress("345 Drum Way");
-            alex.setPhone("34567");
-            alex.setPostal_code("333-333-2345");
+            alex.setPhone("333-333-2345");
+            alex.setPostal_code("23456");
             alex.setDivision(divisionRepository.findAll().get(3));
 
             Customer paulo = new Customer();
             paulo.setFirstName("Paulo");
             paulo.setLastName("Gregoletto");
             paulo.setAddress("456 Bass Court");
-            paulo.setPhone("45678");
-            paulo.setPostal_code("444-222-3456");
+            paulo.setPhone("44-22-3456");
+            paulo.setPostal_code("45678");
             paulo.setDivision(divisionRepository.findAll().get(4));
 
             Customer jason = new Customer();
             jason.setFirstName("Jason");
             jason.setLastName("Suecoff");
             jason.setAddress("567 Studio Drive");
-            jason.setPhone("56789");
-            jason.setPostal_code("555-111-4567");
+            jason.setPhone("555-111-4567");
+            jason.setPostal_code("56789");
             jason.setDivision(divisionRepository.findAll().get(5));
 
             customerRepository.save(matt);
